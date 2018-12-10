8ba0e4b7b6f7fe72d320351cb853c98b867e8de0
Aleksandar <s172375@student.dtu.dk>
2018-12-10 22:01:28 +0100

    added network analysis

---
+++ src/components/NetworkOverTime.vue
@@ -0,0 +1,80 @@
+<template>
+  <b-row class="network-over-time">
+      <h3>{{text}}</h3>
+    <b-row>
+      <b-col cols="6">
+        <img class="imageNet" :src="getNetworkImage()">
+      </b-col>
+      <b-col cols="6">
+        <img class="imagePlot" :src="getDegreeImage()" >
+      </b-col>
+    </b-row>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: "Network",
+  components: {},
+  props: {
+      text:String,
+      fileDegree:String,
+      fileNetwork:String
+  },
+  data() {
+    return {
+        networkImagePath:"../assets/network/",
+        text: this.text,
+        fileDegree: this.fileDegree,
+        fileNetwork: this.fileNetwork
+    };
+  },
+  methods: {
+      getNetworkImage(){
+        console.log("fileNetwork: " + this.fileNetwork)
+        var images = require.context("../assets/network/", false, /\.png$/);
+        return images("./" + this.fileNetwork);
+      },
+
+      getDegreeImage(){
+        console.log("fileDegree: " + this.fileDegree)
+        var images = require.context("../assets/network/", false, /\.png$/);
+        return images("./" + this.fileDegree);
+      }
+  }
+};
+</script>
+<style lang="scss" scoped>
+  .network-display{
+    margin-top: 20px;
+    padding: 20px;
+
+  }
+
+  .imageNet {
+    margin: auto;
+    margin-left: -5%;
+    display: block;
+    max-width: 80%;
+    height: auto;
+    width: auto\9; 
+  }
+
+  .imagePlot {
+    margin: auto;
+    margin-left: -20%;
+    display: block;
+    max-width: 150%;
+    height: 100%;
+    width: auto\9; 
+  }
+
+  h3 {
+    margin-top: 50px;
+    margin-bottom: 20px;
+    font-size: 25px;
+    text-align: center;
+    margin-left: 38%;
+  }
+</style>
+
--- src/views/NetworkAnalysis.vue
+++ src/views/NetworkAnalysis.vue
@@ -1,20 +1,158 @@
 <template>
-    <div class="network">
+    <div class="home">
         {{network}}
+        <div class="container">
+          <b-row>
+            <b-col>
+              <h2> Westworld network</h2>
+              <p> In the figure below the network containing all of the characters in the show is displayed. Each one of the nodes is a character in the show. The red nodes are the hosts and the blue nodes are the humans. The edges are the connections between the characters throughout the show. </p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <img class="network" src="../assets/network/Network of hosts and people.png">
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <p> As we can see the network in westworld is fairly interconnected. Since the stage for the show is a so called theme park, where hosts and humans are free to roam and interact with each other, it is logical that the characters will inevitably cross paths and connect. </p>
+              <h2> Network degree distribution</h2>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <img class="network" src="../assets/network/Degree plot for each character.png">
+              <p>From the graph we can see that the nodes with the most connections are <b>Teddy</b> and <b>Dolores</b>, since they are 2 of the main characters. They both have <b>17</b> connections. The 3 nodes with the highest eigenvector centrality are <b>Teddy</b>, <b>Dolores</b> and <b>Robert</b>. The 3 nodes with the highest betweenness centrality are <b>Stubbs</b>, <b>Teddy</b> and <b>Dolores</b>.
+              </p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <h2> Network evolution over time</h2>
+              <p> With the following set of graphs and plots we can see how the network of characters evolve throughout the show over time. From the network figures it is easy to see how the show sometimes focuses on the host on certain episodes and sometimes on the humans. From the plots we can see who are the characters that the show focuses most for any given episode.</p>
+            </b-col>
+          </b-row>
+          <network-over-time v-for="e in episodes" :key="e.text" :fileDegree="e.fileDegree" :fileNetwork="e.fileNetwork" :text="e.text"></network-over-time>
+        </div>
     </div>
 </template>
 
 <script>
+import NetworkOverTime from "@/components/NetworkOverTime.vue"
 export default {
-  name: "network",
-  components: {},
+  name: "Network",
+  components: {
+    NetworkOverTime
+  },
   data() {
     return {
-      network: "Network Analysis view"
+      networksss: "",
+      episodes: [
+        {
+          text: "Season 01 - Episode 01",
+          fileDegree: "Degree plot s01 episode 1.png",
+          fileNetwork: "Network s01 episode 1.png"
+        },{
+          text: "Season 01 - Episode 02",
+          fileDegree: "Degree plot s01 episode 2.png",
+          fileNetwork: "Network s01 episode 2.png"
+        },{
+          text: "Season 01 - Episode 03",
+          fileDegree: "Degree plot s01 episode 3.png",
+          fileNetwork: "Network s01 episode 3.png"
+        },{
+          text: "Season 01 - Episode 04",
+          fileDegree: "Degree plot s01 episode 4.png",
+          fileNetwork: "Network s01 episode 4.png"
+        },{
+          text: "Season 01 - Episode 05",
+          fileDegree: "Degree plot s01 episode 5.png",
+          fileNetwork: "Network s01 episode 5.png"
+        },{
+          text: "Season 01 - Episode 06",
+          fileDegree: "Degree plot s01 episode 6.png",
+          fileNetwork: "Network s01 episode 6.png"
+        },{
+          text: "Season 01 - Episode 07",
+          fileDegree: "Degree plot s01 episode 7.png",
+          fileNetwork: "Network s01 episode 7.png"
+        },{
+          text: "Season 01 - Episode 08",
+          fileDegree: "Degree plot s01 episode 8.png",
+          fileNetwork: "Network s01 episode 8.png"
+        },{
+          text: "Season 01 - Episode 09",
+          fileDegree: "Degree plot s01 episode 9.png",
+          fileNetwork: "Network s01 episode 9.png"
+        },{
+          text: "Season 01 - Episode 10",
+          fileDegree: "Degree plot s01 episode 10.png",
+          fileNetwork: "Network s01 episode 10.png"
+        },{
+          text: "Season 02 - Episode 01",
+          fileDegree: "Degree plot s02 episode 1.png",
+          fileNetwork: "Network s02 episode 1.png"
+        },{
+          text: "Season 02 - Episode 02",
+          fileDegree: "Degree plot s02 episode 2.png",
+          fileNetwork: "Network s02 episode 2.png"
+        },{
+          text: "Season 02 - Episode 03",
+          fileDegree: "Degree plot s02 episode 3.png",
+          fileNetwork: "Network s02 episode 3.png"
+        },{
+          text: "Season 02 - Episode 04",
+          fileDegree: "Degree plot s02 episode 4.png",
+          fileNetwork: "Network s02 episode 4.png"
+        },{
+          text: "Season 02 - Episode 05",
+          fileDegree: "Degree plot s02 episode 5.png",
+          fileNetwork: "Network s02 episode 5.png"
+        },{
+          text: "Season 02 - Episode 06",
+          fileDegree: "Degree plot s02 episode 6.png",
+          fileNetwork: "Network s02 episode 6.png"
+        },{
+          text: "Season 02 - Episode 07",
+          fileDegree: "Degree plot s02 episode 7.png",
+          fileNetwork: "Network s02 episode 7.png"
+        },{
+          text: "Season 02 - Episode 08",
+          fileDegree: "Degree plot s02 episode 8.png",
+          fileNetwork: "Network s02 episode 8.png"
+        },{
+          text: "Season 02 - Episode 09",
+          fileDegree: "Degree plot s02 episode 9.png",
+          fileNetwork: "Network s02 episode 9.png"
+        },{
+          text: "Season 02 - Episode 10",
+          fileDegree: "Degree plot s02 episode 10.png",
+          fileNetwork: "Network s02 episode 10.png"
+        },
+      ]
     };
   }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  h1 {
+    margin:30px;
+    font-size: 35px;
+    text-transform: uppercase;
+  }
+
+  .network {
+    max-width: 1000px;
+    display: block;
+    height: auto;
+    width: auto\9; 
+  }
+
+  h2 {
+    margin-top: 50px;
+    font-size: 25px;
+    text-transform: uppercase;
+  }
+
 </style>
